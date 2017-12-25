5146776956c23195e9796c7fe0b8170f986d705e
Luke Harris <luke.e.harris@gmail.com>
2017-12-25 14:27:49 -0600

    added modules for budget, user interface, and app controller

---
+++ app.js
@@ -0,0 +1,16 @@
+// Budget module - add items to data structure
+var budgetController = (function() {
+
+})();
+
+
+// UI Module - get input values, add new items to UI, update UI
+var uiController = (fucnction() {
+
+})();
+
+
+// Controller module - event handlers
+var appController = (function(budegetCtrl, uiCtrl) {
+
+})(budgetController, uiController);
