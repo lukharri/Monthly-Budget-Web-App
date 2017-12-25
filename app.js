7bab322c3befed671fd7205a4fff0cba355591d0
Luke Harris <luke.e.harris@gmail.com>
2017-12-25 17:10:50 -0600

    created function for getting budget entry input

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-// Budget module - add items to data structure
+// BUDGET MODULE - add items to data structure
 var budgetController = (function() {
 
 })();
 
 
-// UI Module - get input values, add new items to UI, update UI
+// UI MODULE - get input values, add new items to UI, update UI
 var uiController = (function() {
+	
+	var DOMStrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn'
+	};
 
+
+	return {
+		getInput: function() {
+			return {
+				type: document.querySelector(DOMStrings.inputType).value,
+				descripton: document.querySelector(DOMStrings.inputDescription).value,
+				value: document.querySelector(DOMStrings.inputValue).value
+			};
+		},
+
+		getDOMStrings: function() {
+			return DOMStrings;
+		}
+	};
+	
 })();
 
 
-// Controller module - event handlers
+// CONTROLLER MODULE - event handlers
 var appController = (function(budegetCtrl, uiCtrl) {
+
+	var domStrings = uiController.getDOMStrings();
 
 	var ctrlAddItem = function() {
 		// 1. get the input data
-
+		var input = uiController.getInput();
+		console.log(input);
 		// 2. add item to budget controller
 
 		// 3. add item to UI
@@ -25,7 +50,7 @@ var appController = (function(budegetCtrl, uiCtrl) {
 		// 5. display budget on UI
 	}
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(domStrings.inputBtn).addEventListener('click', ctrlAddItem);
 
 	document.addEventListener('keypress', function(event) {
 		if(event.keyCode === 13 || event.which === 13) {
