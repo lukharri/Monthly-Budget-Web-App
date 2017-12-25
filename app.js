d74e5d0419efa98258910dcc7d84c5412adb10b3
Luke Harris <luke.e.harris@gmail.com>
2017-12-25 15:29:11 -0600

    added event listeners and function template for adding an item to the budget

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ var budgetController = (function() {
 
 
 // UI Module - get input values, add new items to UI, update UI
-var uiController = (fucnction() {
+var uiController = (function() {
 
 })();
 
 
 // Controller module - event handlers
 var appController = (function(budegetCtrl, uiCtrl) {
+
+	var ctrlAddItem = function() {
+		// 1. get the input data
+
+		// 2. add item to budget controller
+
+		// 3. add item to UI
+
+		// 4. caculate budget
+
+		// 5. display budget on UI
+	}
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+		if(event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+	});
 
 })(budgetController, uiController);
